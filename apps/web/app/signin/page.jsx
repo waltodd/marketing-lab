09d7d35524e127b404ddb28e9792b81f60153156
waltodd <waltodd@hotmail.com>
2024-09-05 00:17:45 +0100

    Fix:tailwind bug

--- apps/web/app/signin/page.jsx
+++ apps/web/app/signin/page.jsx
@@ -9,19 +9,22 @@ const Page = () => {
     <div className="w-full flex flex-col min-h-screen justify-center items-center">
       <Card className="px-6 py-4 m-4 w-[350px]">
         <div className="grid w-full max-w-sm items-center gap-1.5 py-2">
-          <Label htmlFor="email">Email</Label>
-          <Input type="email" id="email" placeholder="Email" />
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </div>
         <div className="grid w-full max-w-sm items-center gap-1.5 py-2">
           <Label htmlFor="password">Password</Label>
           <Input type="password" id="password" placeholder="Password" />
         </div>
         <div className="grid w-full items-center max-w-sm items-center gap-1.5 py-2">
-        <Button className="w-full" variant="outline">Login</Button>
+          <Button className="w-full" variant="outline">
+            Login
+          </Button>
         </div>
 
         <div className="grid w-full max-w-sm items-center gap-1.5 py-2">
-        <p>Não tem uma conta? <Link href="/signup">Registre-se</Link> agora.</p>
+          <p>
+            Não tem uma conta? <Link href="/signup">Registre-se</Link> agora.
+          </p>
         </div>
       </Card>
     </div>
