40376e26311b9553747d39fd290e6ab0de4ccaa8
waltodd <waltodd@hotmail.com>
2024-09-05 00:17:45 +0100

    Fix:tailwind bug

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,12 +1,8 @@
-import path from 'path';
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.resolve.alias['@/components'] = path.join(__dirname, 'components');
-    config.resolve.alias['@utils'] = path.join(__dirname, 'utils');
-    return config;
-  }
+  
 };
 
 export default nextConfig;
