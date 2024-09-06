83d7c12bbb79bd1184cbb660374dbb3155445b40
waltodd <waltodd@hotmail.com>
2024-09-06 02:12:47 +0100

    middleware added

--- apps/web/app/dashboard/admin/page.tsx
+++ apps/web/app/dashboard/admin/page.tsx
@@ -1,22 +1,30 @@
 "use client";
-
+import Cookies from "js-cookie";
 import React, { useState, useEffect } from "react";
 import { db } from "@/pages/api/instant";
-
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 const Page = () => {
   const { isLoading, user, error } = db.useAuth();
   const userId = user?.id;
-
+  const router = useRouter();
   if (isLoading) {
     return <div>Loading...</div>;
   }
   if (error) {
     return <div>Uh oh! {error.message}</div>;
   }
+
+  const handleLogOut = () =>{
+    Cookies.remove("accessToken");
+    router.push("/");
+  }
   return (
     <div>
       DASHBOARD
       {user ? <p className="text-white">Welcome, {user.email}!</p> : <p>Loading...</p>}
+       <Button onClick={handleLogOut} className=" border-none  bg-[#80D77E] text-white"
+            variant="outline">Logout</Button>
     </div>
   );
 };
---
+++ apps/web/middleware.ts
@@ -0,0 +1,30 @@
+// middleware.ts
+import { NextResponse, NextRequest } from 'next/server';
+import jwt from 'jsonwebtoken';
+
+const PUBLIC_ROUTES = ['/signin', '/signup', '/code', '/'];
+const secretKey = process.env.JWT_SECRET || 'your-secret-key';
+
+export function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  // Allow access to public routes
+  if (PUBLIC_ROUTES.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  // Get the token from cookies for protected routes
+  const token = req.cookies.get('accessToken')?.value;
+
+  
+
+  // If no token is found, redirect to the signin page
+  if (!token) {
+    return NextResponse.redirect(new URL('/signin', req.url));
+  }
+}
+
+// Apply middleware only to specific routes (e.g., /dashboard and /profile)
+export const config = {
+  matcher: ['/dashboard/:path*', '/profile/:path*'],
+};
